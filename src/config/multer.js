c825fd839e3ed4f43b82e5d483e230e03a3ae1cd
HenriqueRodriguesRossi <hrodriguescruciakrossi46@gmail.com>
2023-10-24 12:09:42 -0300

    Configurando o multer

---
+++ src/config/multer.js
@@ -0,0 +1,12 @@
+const multer = require("multer")
+const path = require("path")
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback){
+        callback(null, "uploads/")
+    },
+
+    filename: function(req, file, callback){
+        callback(null, Date.now() + path.extname(file.originalname))
+    }
+})
